34f2d1b7dcec3376dfbb8cfa7bc6bacb13311c33
ishrat144 <81430061+ishrat144@users.noreply.github.com>
2021-11-09 12:56:45 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Users from './Component/Users'
+import User1 from './Component/User1'
+import User2 from './User2'
+
+const App = () => {
+  return (
+    <>
+    <User1 />
+      <Users />
+      <User2 />
+    </>
+  )
+}
+
+export default App
---
+++ User2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const pi= 3.1415962;
+function Triplepi() {
+    return(
+    <ul><li>{pi + 3}</li></ul>
+    ); 
+}
+        
+export default Triplepi;
